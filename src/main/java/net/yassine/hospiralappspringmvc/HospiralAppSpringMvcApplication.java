3a66c694503d7f61cb966b2cd9d959d2acbe5dd6
yassine4real <yssnabbou2002@gmail.com>
2024-11-29 18:34:16 +0100

    first commit

---
+++ src/main/java/net/yassine/hospiralappspringmvc/HospiralAppSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package net.yassine.hospiralappspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospiralAppSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospiralAppSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yassine/hospiralappspringmvc/HospiralAppSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yassine.hospiralappspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospiralAppSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
