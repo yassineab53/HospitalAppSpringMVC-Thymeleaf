fb1c3e0fa28600d9cb3eeda3065a2fb702c70d3c
yassine4real <yssnabbou2002@gmail.com>
2024-11-29 23:38:38 +0100

    voir les 3 methodes pour creer les objets

--- src/main/java/net/yassine/hospiralappspringmvc/HospiralAppSpringMvcApplication.java
+++ src/main/java/net/yassine/hospiralappspringmvc/HospiralAppSpringMvcApplication.java
@@ -1,13 +1,55 @@
 package net.yassine.hospiralappspringmvc;
 
+import net.yassine.hospiralappspringmvc.entities.Patient;
+import net.yassine.hospiralappspringmvc.repository.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class HospiralAppSpringMvcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(HospiralAppSpringMvcApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner start(PatientRepository patientRepository){
+        /*return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                //code
+            }
+        };*/
+        return args -> {
+            //premier solution pour creer des objets : NoArgsConstructor
+            Patient p1 = new Patient();
+            p1.setNom("Abbou");
+            p1.setPrenom("Yassine");
+            p1.setScore((100));
+            p1.setMalade(false);
+            p1.setDateNaissance(new Date());
+
+            //2eme solution pour creer des objets : AllArgsConstructor
+            Patient p2 = new Patient(null, "Abbou", "Aymane", new Date(), 50, false);
+
+            //3eme solution : en utilisant @Builder : design pattern
+            Patient p3 = Patient.builder()
+                    .nom("Hassani")
+                    .prenom("Achraf")
+                    .dateNaissance(new Date())
+                    .malade(false)
+                    .score(10)
+                    .build();
+
+            patientRepository.save(p1);
+            patientRepository.save(p2);
+            patientRepository.save(p3);
+        };
     }
 
 }
