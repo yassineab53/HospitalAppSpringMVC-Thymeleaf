acbecffa0252789725dde2f6fa9d49e6498b73d6
yassine4real <yssnabbou2002@gmail.com>
2024-11-29 18:43:22 +0100

    add Lombok annotations for Patient class

--- src/main/java/net/yassine/hospiralappspringmvc/entities/Patient.java
+++ src/main/java/net/yassine/hospiralappspringmvc/entities/Patient.java
@@ -1,10 +1,13 @@
 package net.yassine.hospiralappspringmvc.entities;
 
+import lombok.*;
+
 import java.util.Date;
 
 /**
  * @author $ {USER}
  **/
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter @ToString @Builder
 public class Patient {
     private Long id;
     private String nom;
