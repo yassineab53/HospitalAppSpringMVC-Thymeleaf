9af0f9867375d61d4bd2a13aeb5bcf82f131965b
yassine4real <yssnabbou2002@gmail.com>
2024-11-29 18:40:20 +0100

    add attributes to Patient class

--- src/main/java/net/yassine/hospiralappspringmvc/entities/Patient.java
+++ src/main/java/net/yassine/hospiralappspringmvc/entities/Patient.java
@@ -1,7 +1,15 @@
 package net.yassine.hospiralappspringmvc.entities;
 
+import java.util.Date;
+
 /**
  * @author $ {USER}
  **/
 public class Patient {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private Date dateNaissance;
+    private int score;
+    private boolean malade;
 }
