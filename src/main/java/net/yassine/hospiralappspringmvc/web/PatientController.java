b0486692440f2dca423c6ba05c50642019d35a58
yassine4real <yssnabbou2002@gmail.com>
2024-12-09 23:23:33 +0100

    notre controlleur a besoin d'acceder à la base de donnee donc on va déclarer un objet de type PatientRepository

--- src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
+++ src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
@@ -1,7 +1,9 @@
 package net.yassine.hospiralappspringmvc.web;
 
+import net.yassine.hospiralappspringmvc.repository.PatientRepository;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PatientController {
+    private PatientRepository patientRepository;
 }
