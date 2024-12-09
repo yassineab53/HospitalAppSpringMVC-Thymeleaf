062b6f6838286d201788991d60115a9429dc357b
yassine4real <yssnabbou2002@gmail.com>
2024-12-09 23:29:32 +0100

    ajouter la method index qui retourne un vue : patients.html

--- src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
+++ src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
@@ -1,13 +1,21 @@
 package net.yassine.hospiralappspringmvc.web;
 
 import net.yassine.hospiralappspringmvc.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PatientController {
+    @Autowired
     private PatientRepository patientRepository;
-    //injection des dependances via le constructeur
-    public PatientController(PatientRepository patientRepository) {
+    //injection des dependances via le constructeur   ou en utilisant l'annotation Autowired
+    /*public PatientController(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }*/
+
+    @GetMapping("/index")
+    public String index(){
+        return "patients";
     }
 }
