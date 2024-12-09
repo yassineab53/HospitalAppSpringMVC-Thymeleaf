dcf9d89368605f2adba36196acbb5685c7f41e9b
yassine4real <yssnabbou2002@gmail.com>
2024-12-10 00:21:55 +0100

    afichage en utilisant un tableau

--- src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
+++ src/main/java/net/yassine/hospiralappspringmvc/web/PatientController.java
@@ -1,9 +1,13 @@
 package net.yassine.hospiralappspringmvc.web;
 
+import net.yassine.hospiralappspringmvc.entities.Patient;
 import net.yassine.hospiralappspringmvc.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PatientController {
@@ -15,7 +19,10 @@ public class PatientController {
     }*/
 
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("listPatients", patients);
+
         return "patients";
     }
 }
